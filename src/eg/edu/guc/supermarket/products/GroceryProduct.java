6ad8734f1c741394f7b12bcfec4e26177ab2abdc
GPhantom <shadytantawy@protonmail.com>
2017-02-12 20:05:37 +0200

    [+] Added GroceryProduct Class

---
+++ src/eg/edu/guc/supermarket/products/GroceryProduct.java
@@ -0,0 +1,34 @@
+package eg.edu.guc.supermarket.products;
+
+public class GroceryProduct {
+	private final String name;
+	private double price;
+	private int discount;
+	
+	public GroceryProduct(String name, double price, int discount){
+		this.name     = name;
+		this.price    = price;
+		this.discount = discount;
+	}
+	public String toString(){
+		return "Name " + this.name +
+				"Price " + this.price +
+				"Discount " + this.discount;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+	public String getName() {
+		return name;
+	}
+	
+}
