857ee590d700f02ac4914ab4a12557b569150fbe
GPhantom <shadytantawy@protonmail.com>
2017-02-12 20:06:22 +0200

    [+] Added DairyProduct Class

---
+++ src/eg/edu/guc/supermarket/products/DairyProduct.java
@@ -0,0 +1,15 @@
+package eg.edu.guc.supermarket.products;
+
+public class DairyProduct extends GroceryProduct{
+	FatLevel f;
+	
+	public DairyProduct(String name, double price, int discount, FatLevel f){
+		super(name, price, discount);
+		this.f = f;
+	}
+	
+	public String toString(){
+		return super.toString() + "FatLevel " + this.f;
+	}
+
+}
